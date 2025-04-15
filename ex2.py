935633f0ac3a0b009699e3763262fb04da286fb6
Ricardo Chambel <ricardo.chambel07@gmail.com>
2025-04-15 12:41:26 +0100

    NEW-FEATURE-ex2

--- ex2.py
+++ ex2.py
@@ -1,8 +1,6 @@
 # Pedir um numero, e analisar se o mesmo é primo (só if's)
 num = int(input("Insira um numero:"))
-if num%2==0 and num!=2:
-    print("Numero não é primo")
-elif num%3==0 and num!=3:
+if (num%2==0 and num!=2) or (num%3==0 and num!=3):
     print("Numero não é primo")
 else:
     print("Numero é primo")

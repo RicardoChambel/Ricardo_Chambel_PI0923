47222485988d7a09ee83306c90b2ff34202e3e02
Ricardo Chambel <ricardo.chambel07@gmail.com>
2025-04-15 14:36:15 +0100

    New-materia

---
+++ materia_15-04-2025/1.py
@@ -0,0 +1,13 @@
+num = [1, 2, 3, 4, 5, 6]
+
+num.append(7) # Adiciona '7' na ultima posição da lista
+
+for i in num:
+    print(i, end=" ")
+print("\n")
+
+num.insert(0, 8) #Adicionar '8' na posição de index '0' da lista
+
+for i in num:
+    print(i, end=" ")
+print("\n")
---
+++ materia_15-04-2025/2.py
@@ -0,0 +1,26 @@
+#nomes = ["Daniel", "Daniel", "Daniel", "Daniel"]
+nomes = []
+
+#for nome in nomes:
+#    print(nome)
+
+#for nome in nomes:
+#    nomes.append(input("Introduza o seu nome: "))
+
+#para = True
+
+#while para:
+#    dec = input("Quer continuar ? (S ou N)\n->")
+#    if dec == "S" or dec == "s":
+#        para = False
+#        continue
+#    nomes.append(input("Inserir um nome: "))
+
+#while True:
+#    dec = input("Quer parar? (S ou N)\n->")
+#    if dec == "S" or dec == "s":
+#        break
+#    nomes.append(input("Inserir um nome: "))
+
+#for nom in nomes:
+#    print(nom)

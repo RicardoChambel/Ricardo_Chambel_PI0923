7cc155ccccefccd3a59bea03bdac1322c284a40e
Ricardo Chambel <90462393+RicardoChambel@users.noreply.github.com>
2025-05-13 15:11:52 +0100

    Update verify.py

--- UFCD10789/Python/TrainingServer_ID_Challenge/verify.py
+++ UFCD10789/Python/TrainingServer_ID_Challenge/verify.py
@@ -1,15 +1,43 @@
+# RESOLUCAO MINHA:
+# TRANSFORMAR A RESPOSTA DO URL PARA UM .TXT COM ENCODING CORRETO
+# DE SEGUIDA
+# TRANSFORMAR O TEXTO .TXT PARA UM .JSON
+# VERIFICAR DESCRIÇÕES TODAS DO FICHEIRO .JSON
+# ENCONTRAR O FICHEIRO CUJAS DESCRIÇÕES TODAS TENHAM ROGÉLIO 
+
 import requests
 
 ficheiros_ids = ["id_1.txt","id_2.txt","id_3.txt","id_4.txt","id_5.txt"]
+user_ids = []
 
-with open("ids_encontrados.txt", "w") as f:
-    for ficheiro in ficheiros_ids:
-        with open(ficheiro, "r") as f:
-            for linha in f:
-                linha = linha.strip()
-                if linha.isdigit():
-                    f.write(str(linha) + "\n")
+# METER OS IDS TODOS NO FICHEIRO DE IDS ENCONTRADOS
+with open("ids_encontrados.txt", "r") as f:
+    for linha in f:
+        linha = linha.strip()
+        if linha.isdigit():
+            user_ids.append(int(linha))
 
+print(user_ids)
+
+#id = 7194
+#f = open("id_1.txt", "a")
+#for id in range(7194, 8000):
+#  print("---- A procurar no ID: ", id)
+#  resposta = requests.get(f'https://trainingserver.atec.pt/TrainingServer/Mulberry/JSON/Controls/Calendar/getCalendarDataSource.ashx?command=_SelectAllSchedulesDataSetGivenByUserId&oId={id}&idField=DataValueField&titleField=DataTextField&startDateField=DataStartField&endDateField=DataEndField&backgroundColorField=&textColorField=textcolor&eventColorField=color&description=description&picField=pic&urlField=url&start=1747004400&end=1747609200&_=1747127413405')
+
+#  if "Sessão como Formador" in resposta.text and ("Rogelio" in resposta.text or "Rogélio" in resposta.text):
+#    user_ids.append(id)
+#    f.write(str(id)+'\n')
+#  else:
+#    print("[x] O Formador Rogélio não foi encontrado como formador no horario do ID: ", id)
+
+#f.close
+#print("-- ID's Encontrados --")
+#for i in user_ids:
+#  print(i,'\n')
+
+
+# TRANSFORMAR RESPOSTA PARA .TXT COM ENCODING CORRETO
 #id = 9838
 #f = open("verify.txt", "wb")
 #print("---- A importar o request do ID ", id)

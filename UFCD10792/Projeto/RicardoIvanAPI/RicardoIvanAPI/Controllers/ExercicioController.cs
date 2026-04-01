867c4e0fa0b3bf0c3d465ec2894d9e79906e2326
Ricardo Chambel <ricardo.chambel07@gmail.com>
2026-04-01 12:42:31 +0100

    Atualizar Projeto

---
+++ UFCD10792/Projeto/RicardoIvanAPI/RicardoIvanAPI/Controllers/ExercicioController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RicardoIvanAPI.Data;
+using System.Net.Http;
+
+namespace RicardoIvanAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RicardoController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        private readonly IHttpClientFactory _clientFactory;
+
+        public RicardoController(ApiContext context, IHttpClientFactory clientFactory)
+        {
+            _context = context;
+            _clientFactory = clientFactory;
+        }
+
+        [HttpGet("request-mountebank")]
+        public async Task<IActionResult> GetFromMountebank()
+        {
+            var client = _clientFactory.CreateClient();
+
+            string url = "http://localhost:2525";
+
+            try
+            {
+                var response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    return Ok(new { msg = "Lido do Mountebank!", dados = content });
+                }
+                return BadRequest("Mountebank offline ou porta errada.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Erro: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ UFCD10792/Projeto/RicardoIvanAPI/RicardoIvanAPI/Data/APIcontext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RicardoIvanAPI.Models;
+using System.Collections.Generic;
+
+namespace RicardoIvanAPI.Data
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
+        public DbSet<Item> Items { get; set; }
+    }
+}
